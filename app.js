a90af80b3cbd017d4c5f47249e782425f18bd5ac
Sukeesan <sukeesank@gmail.com>
2022-08-01 17:50:32 -0400

    update html/js and add css

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-function start () {
-
-// readup on Javacript promises
-const requestURL = "https://pokeapi.co/api/v2/pokemon/charizard";
-fetch(requestURL)
-.then((x) => x.json())
-.then((obj) => {
-    console.log(obj); 
+function displayPokemon(obj) {
+    //Name
     const name = obj.name;
     const pokemonNameMessage = "Pokemon Name: " + name;
     document.getElementById("name").innerHTML = pokemonNameMessage
@@ -25,7 +19,16 @@ let move4 = obj.moves[3].move.name;
 document.getElementById(
     "move-list"
 ).innerHTML = `<li>${move1}</li> <li>${move2}</li> <li>${move3}</li> <li>${move4}</li>` 
-});
-
 }
 
+
+function fetchPokemon() {
+const requestURL = "https://pokeapi.co/api/v2/pokemon/";
+const searchTerm = document.getElementById("search-box").value;
+console.log(searchTerm);
+fetch(requestURL + searchTerm)
+        .then((x) => x.json())
+        .then((obj) => {
+           displayPokemon(obj); 
+        });
+}
